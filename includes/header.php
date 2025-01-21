b7b0299a8e6eac835c95abd833f96c09a77b6df3
Madushidikrukshika <87822812+Madushidikrukshika@users.noreply.github.com>
2025-01-21 16:06:08 +0530

    title changed

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://cdn.tailwindcss.com"></script>
   <link rel="stylesheet" href="./assetes/css/root.css">
+  <title>Elevate Your Brand with Pride Expo Solutions: Expert Exhibition Stand Design and Build Agency in Sri Lanka</title>
   <meta name="description" content="Pride Expo Solutions: Expert exhibition stand design and build agency in Sri Lanka. Specializing in custom-built and modular display stands for conventions and conferences. Elevate your brand with our skilled team and client-centric approach. From complex expo stands to small display kits, we deliver excellence nationwide.">
     <meta name="keywords" content="Exhibition stand design Sri Lanka, Custom-built expo stands, Modular display stands Sri Lanka, Conference display solutions, Pride Expo Solutions, Expo stand builders Sri Lanka, Exhibition stand agency, Below-the-line marketing Sri Lanka, Custom display kits, Trade show stand design, Convention display stands, Brand building solutions Sri Lanka, Skilled exhibition designers, Expo stand customization, Modular expo stand builders.">
   <link rel="apple-touch-icon" sizes="180x180" href="./assetes/image/favicon_io (2)/apple-touch-icon.png">
